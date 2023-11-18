ca9e57e0f1789abc170647b431ff9d51a4c128e5
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2023-11-18 17:42:37 -0500

    Persistencia

---
+++ src/producto/producto.entity.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { TiendaEntity } from 'src/tienda/tienda.entity';
+
+@Entity()
+export class ProductoEntity {
+@PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ name: string;
+ 
+ @Column()
+ price: number;
+ 
+ @Column()
+ type: string;
+
+ @ManyToMany(() => TiendaEntity, tienda => tienda.productos)
+ tiendas:TiendaEntity[];
+}
---
+++ src/tienda/tienda.entity.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { ProductoEntity } from 'src/producto/producto.entity';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class TiendaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+   
+    @Column()
+    name: string;
+    
+    @Column()
+    city: number;
+    
+    @Column()
+    address: string;  
+
+    @ManyToMany(() => ProductoEntity, producto => producto.tiendas)
+    @JoinTable()
+    productos: ProductoEntity[];
+}
