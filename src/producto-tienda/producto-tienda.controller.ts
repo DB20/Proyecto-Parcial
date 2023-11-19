107f671f76f7c183817dc10866a0c6611721abab
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2023-11-19 07:17:43 -0500

    controlador tienda y asociación

--- src/producto-tienda/producto-tienda.controller.ts
+++ src/producto-tienda/producto-tienda.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+import { ProductoTiendaService } from './producto-tienda.service';
+import { TiendaDto } from 'src/tienda/tienda.dto';
+import { TiendaEntity } from 'src/tienda/tienda.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('producto-tienda')
-export class ProductoTiendaController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class ProductoTiendaController {
+    constructor(private readonly productoTiendaService: ProductoTiendaService){}
+
+    @Post('products/:productoId/stores/:tiendaId')
+    async addStoreToProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
+        return await this.productoTiendaService.addStoreToProduct(productoId, tiendaId);
+    }
+
+   @Get('products/:productoId/stores/:tiendaId')
+   async findStoreFromProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
+       return await this.productoTiendaService.findStoreFromProduct(productoId, tiendaId);
+   }
+   @Get('products/:productoId/stores')
+   async findStoresFromProduct(@Param('productoId') productoId: string){
+       return await this.productoTiendaService.findStoresFromProduct(productoId);
+   }
+
+   @Put('producto/:productoId/stores')
+   async updateStoresFromProduct(@Body() tiendasDto: TiendaDto[], @Param('productoId') productoId: string){
+       const tienda = plainToInstance(TiendaEntity, tiendasDto)
+       return await this.productoTiendaService.updateStoresFromProduct(productoId, tienda);
+   }
+
+   @Delete('products/:productoId/stores/:tiendaId')
+@HttpCode(204)
+   async deleteStoreFromProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
+       return await this.productoTiendaService.deleteStoreFromProduct(productoId, tiendaId);
+   }
+}
--- src/tienda/tienda.controller.ts
+++ src/tienda/tienda.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors} from '@nestjs/common';
+import { TiendaService } from './tienda.service';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+import { TiendaEntity } from './tienda.entity';
+import { TiendaDto } from './tienda.dto';
+
 
 @Controller('tienda')
-export class TiendaController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class TiendaController {
+    constructor(private readonly tiendaService: TiendaService){}
+
+    @Get()
+    async findAll(){
+        return await this.tiendaService.findAll();
+    }
+
+    @Get(':tiendaId')
+    async findOne(@Param('tiendaId') tiendaId: string){
+        return await this.tiendaService.findOne(tiendaId)
+    }
+
+    @Post()
+    async create(@Body() tiendaDTO: TiendaDto) {
+        const producto: TiendaEntity = plainToInstance(TiendaEntity, tiendaDTO);
+        return await this.tiendaService.create(producto);
+    }
+
+    @Put(':tiendaId')
+    async update(@Param('tiendaId') tiendaId: string, @Body() tiendaDTO: TiendaDto) {
+        const producto: TiendaEntity = plainToInstance(TiendaEntity, tiendaDTO);
+        return await this.tiendaService.update(tiendaId, producto);
+    }
+
+    @Delete(':tiendaId')
+    @HttpCode(204)
+    async delete(@Param('tiendaId') tiendaId: string) {
+        return await this.tiendaService.delete(tiendaId);
+    }
+}
