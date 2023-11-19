e242626950a1c4942290e8bc96da957b94bf6f34
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2023-11-19 07:23:24 -0500

    fix asociación modulo

--- src/producto-tienda/producto-tienda.module.ts
+++ src/producto-tienda/producto-tienda.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductoTiendaService } from './producto-tienda.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductoTiendaController } from './producto-tienda.controller';
+import { ProductoEntity } from 'src/producto/producto.entity';
+import { TiendaEntity } from 'src/tienda/tienda.entity';
 
 @Module({
   providers: [ProductoTiendaService],
-  controllers: [ProductoTiendaController]
+  imports: [TypeOrmModule.forFeature([ProductoEntity, TiendaEntity])],
+  controllers: [ProductoTiendaController],
 })
 export class ProductoTiendaModule {}
