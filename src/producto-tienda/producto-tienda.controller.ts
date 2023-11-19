270d697edb743c4c8aed5cba762ecdd85f04c79c
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2023-11-19 14:53:55 -0500

    fix endpoint asociación

--- src/producto-tienda/producto-tienda.controller.ts
+++ src/producto-tienda/producto-tienda.controller.ts
@@ -6,32 +6,32 @@ import { TiendaDto } from 'src/tienda/tienda.dto';
 import { TiendaEntity } from 'src/tienda/tienda.entity';
 import { plainToInstance } from 'class-transformer';
 
-@Controller('producto-tienda')
+@Controller('products')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class ProductoTiendaController {
     constructor(private readonly productoTiendaService: ProductoTiendaService){}
 
-    @Post('products/:productoId/stores/:tiendaId')
+    @Post(':productoId/stores/:tiendaId')
     async addStoreToProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
         return await this.productoTiendaService.addStoreToProduct(productoId, tiendaId);
     }
 
-   @Get('products/:productoId/stores/:tiendaId')
+   @Get(':productoId/stores/:tiendaId')
    async findStoreFromProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
        return await this.productoTiendaService.findStoreFromProduct(productoId, tiendaId);
    }
-   @Get('products/:productoId/stores')
+   @Get(':productoId/stores')
    async findStoresFromProduct(@Param('productoId') productoId: string){
        return await this.productoTiendaService.findStoresFromProduct(productoId);
    }
 
-   @Put('producto/:productoId/stores')
+   @Put(':productoId/stores')
    async updateStoresFromProduct(@Body() tiendasDto: TiendaDto[], @Param('productoId') productoId: string){
        const tienda = plainToInstance(TiendaEntity, tiendasDto)
        return await this.productoTiendaService.updateStoresFromProduct(productoId, tienda);
    }
 
-   @Delete('products/:productoId/stores/:tiendaId')
+   @Delete(':productoId/stores/:tiendaId')
 @HttpCode(204)
    async deleteStoreFromProduct(@Param('productoId') productoId: string, @Param('tiendaId') tiendaId: string){
        return await this.productoTiendaService.deleteStoreFromProduct(productoId, tiendaId);
